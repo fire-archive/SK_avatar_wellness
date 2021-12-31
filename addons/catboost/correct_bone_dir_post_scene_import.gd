a4fe92cfcd3fbc20ac35ccfa0a23baa25d1d3277
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-12-31 09:20:14 -0800

    Tuned the files.

---
+++ addons/catboost/correct_bone_dir_post_scene_import.gd
@@ -0,0 +1,44 @@
+# MIT License
+# 
+# Copyright (c) 2020 K. S. Ernest (iFire) Lee & V-Sekai
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+@tool
+extends EditorScenePostImport
+
+const avatar_editor_const = preload("res://addons/vsk_avatar/vsk_avatar_definition_editor.gd")
+
+func _post_import(scene : Node):
+	var queue : Array
+	queue.push_back(scene)
+	var string_builder : Array
+	while not queue.is_empty():
+		var front = queue.front()
+		var node = front
+		if node is Skeleton3D:
+			avatar_editor_const.correct_bone_directions(scene, node, null, null)
+			avatar_editor_const._refresh_skeleton(node)
+			break
+		var child_count : int = node.get_child_count()
+		for i in child_count:
+			queue.push_back(node.get_child(i))
+		queue.pop_front()	
+	return scene
+	
